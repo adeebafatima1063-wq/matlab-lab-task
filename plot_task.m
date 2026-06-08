3693f3d2d243895401f77e0b8688c87778b0bbd9
adeebafatima1063-wq <adeebafatima1063@gmail.com>
2026-06-08 10:53:17 +0500

    uploaded

---
+++ plot_task.m
@@ -0,0 +1,23 @@
+t=[2:2:10]
+subplot(2,2,1)
+plot(t,exp^(-t),r--d)
+legend('sin')
+xlabel('xaxis')
+ylabel('sin values')
+grid on 
+
+subplot(2,2,2)
+plot(t,sin(t),b--d)
+legend('sin')
+xlabel('xaxis')
+ylabel('sin values')
+grid on 
+legend('y axis')
+
+subplot(2,2,2)
+plot(t,sin(t),b--d)
+legend('sin')
+xlabel('xaxis')
+ylabel('sin values')
+grid on 
+legend('y axis')
---
+++ plot_task2.m
@@ -0,0 +1,7 @@
+t=[2:2:10]
+subplot(2,2,1)
+plot(t,cos(2*t),r--d)
+legend('cos')
+xlabel('xaxis')
+ylabel('cos values')
+grid on 
